074240bc437e5d01923d85f83e126603235e4ba7
braydensoneill <braydensoneill@gmail.com>
2022-08-24 03:24:42 +0100

    Camera Movement | Camera Rotation
    <Camera Movement> Camera gameobject now follows the Player gameobject
    
    <Camera Rotations> Camera rotates around the player based on the position of the mouse inputs

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -16,9 +16,26 @@ namespace BON
         public float mouseY;
 
         private PlayerControls inputActions;
+        CameraHandler cameraHandler;
 
         private Vector2 movementInput;
         private Vector2 cameraInput;
+
+        private void Awake()
+        {
+            cameraHandler = CameraHandler.singleton;
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+
+            if(cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, mouseX, mouseY);
+            }
+        }
 
         public void OnEnable()
         {
--- Assets/Scripts/Player/PlayerLocomotion.cs
+++ Assets/Scripts/Player/PlayerLocomotion.cs
@@ -42,6 +42,7 @@ namespace BON
             moveDirection = cameraObject.forward * inputHandler.vertical;
             moveDirection += cameraObject.right * inputHandler.horizontal;
             moveDirection.Normalize();
+            moveDirection.y = 0;
 
             float speed = movementSpeed;
             moveDirection *= speed;
