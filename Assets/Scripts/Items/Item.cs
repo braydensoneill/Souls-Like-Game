de2ddc34334cdd5a277c1542b0dab9f088a21554
braydensoneill <braydensoneill@gmail.com>
2022-08-25 03:24:14 +0100

    Weapons
    Created the foundations of a equipable weapons system.

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BON
+{
+    public class Item : ScriptableObject
+    {
+        [Header("Item Information")]
+        public Sprite itemIcon;
+        public string itemName;
+    }
+}
---
+++ Assets/Scripts/Items/WeaponItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BON
+{
+    [CreateAssetMenu(menuName = "Items/Weapon Item")]
+    public class WeaponItem : Item
+    {
+        public GameObject modelPrefab;
+        public bool isUnarmed;
+
+    }
+}
