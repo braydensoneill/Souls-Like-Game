147e8814faefd7f34cda7768c02d76628d94a2b3
braydensoneill <braydensoneill@gmail.com>
2022-08-31 23:32:48 +0100

    Bug Fix: Holding Weapons
    Problem: Left handed weapons were being held backwards and some weapons were held off centre.
    
    Solution: Changed left hand override position + rotation and fixed multiple weapon pivot's position + rotation

---
+++ Assets/Scripts/Interface/ManaBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BON
+{
+    public class ManaBar : MonoBehaviour
+    {
+        public Slider slider;
+
+        public void SetBarMaxMana(float maxValue)
+        {
+            slider.maxValue = maxValue;
+            slider.value = maxValue;
+        }
+
+        public void SetBarCurrentMana(float currentValue)
+        {
+            slider.value = currentValue;
+        }
+    }
+}
